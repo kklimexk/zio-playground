6c48932ee44e7939381c3167672cdb6f03b1f3b7
kklimexk <klimekkrzys@gmail.com>
2020-05-31 17:20:48 +0200

    updated zio version

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ version := "0.1"
 
 scalaVersion := "2.12.11"
 
-lazy val zioVersion = "1.0.0-RC19"
+lazy val zioVersion = "1.0.0-RC20"
 lazy val sparkVersion = "2.4.5"
 lazy val deltaVersion = "0.6.0"
 
--- src/main/scala/zio/examples/HelloZio.scala
+++ src/main/scala/zio/examples/HelloZio.scala
@@ -1,19 +1,16 @@
 package zio.examples
 
-import java.io.IOException
-
-import zio.ZIO
 import zio.console._
 
 object HelloZio extends zio.App {
 
-  def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] =
-    myAppLogic.fold(_ => 1, _ => 0)
+  def run(args: List[String]) =
+    myAppLogic.exitCode
 
-  val myAppLogic: ZIO[Console, IOException, Unit] =
+  val myAppLogic =
     for {
-      _    <- putStrLn("Hello! What is your name?")
+      _ <- putStrLn("Hello! What is your name?")
       name <- getStrLn
-      _    <- putStrLn(s"Hello, ${name}, welcome to ZIO!")
+      _ <- putStrLn(s"Hello, $name, welcome to ZIO!")
     } yield ()
 }
